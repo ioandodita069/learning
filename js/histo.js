aab39cb43956711c788ce044a014a8f93308c41d
pancudaniel7 <pancudaniel7@gmail.com>
2022-04-20 23:40:28 +0300

    Histo

---
+++ js/histo.js
@@ -0,0 +1,10 @@
+let text = "Ioan are mere!"
+
+let histoLine = ""
+for(let i=0;i<text.length+2;i++){
+	histoLine+="*"
+}
+
+console.log(histoLine)
+console.log("*" + text + "*")
+console.log(histoLine)
