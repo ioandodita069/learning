108cdea1a86ff19d5af6624c425c42b4272d49de
ioan <ioandodita069@gmail.com>
2022-04-28 23:26:48 +0300

    Java Script typeof,type conversion and problems

---
+++ js/typeof.js
@@ -0,0 +1,7 @@
+console.log("ioan".constructor + "<br>" +
+    (9.99).constructor + "<br>" +
+    false.constructor + "<br>" +
+    [1, , 4, 4].constructor + "<br>" +
+    { name: 'ioan', age: 21 }.constructor + "<br>" +
+    new Date().constructor + "<br>" +
+    function () { }.constructor)
