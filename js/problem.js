5f36402ca09f96f712b53d5b7cdb74f837ea5866
ioan <ioandodita069@gmail.com>
2022-04-28 23:29:09 +0300

    Java Script typeof,type conversion and problems

---
+++ js/problem.js
@@ -0,0 +1,16 @@
+test("Ana are ce nu are Maria.");
+function test(e) {
+    var words = e
+        .toLowerCase()
+        .split("")
+
+    var consonant = words
+        .filter(el => !"aiueo"
+            .split("")
+            .includes(el)
+        ).sort((a, b) =>
+            a.localeCompare(b)
+        ).join('')
+
+    console.log(consonant)
+}
