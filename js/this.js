2a92c21d7bfdb91f235e5ea21ca466264f2728fe
Ioan <94746070+ioandodita069@users.noreply.github.com>
2022-05-09 19:45:36 +0300

    Delete this.js

--- js/this.js
+++
@@ -1,9 +0,0 @@
-const person = {
-    firstName: "Ioan",
-    lastName: "Dodita",
-    id: 5010212270831,
-    details: function () {
-        return this.firstName + "/ " + this.lastName + "/" + this.id;
-    }
-}
-console.log(person.details())
