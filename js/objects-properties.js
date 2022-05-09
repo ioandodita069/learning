4e527c855b818b63098c6458aa204f8ba13cfcbc
ioan <ioandodita069@gmail.com>
2022-05-09 19:40:55 +0300

    Java Script classes method;object properties and arrow functions

---
+++ js/objects-properties.js
@@ -0,0 +1,13 @@
+const myFarm = {
+    name: "Dodita Ioan",
+    age: 21,
+    animals: {
+        animal1: "cows",
+        animal2: "sheeps",
+        animal3: "horses"
+    }
+}
+// console.log(myFarm.animals.animal2);
+// console.log(myFarm.animals["animal1"]);
+console.log(myFarm["animals"]["animal3"]);
+// you can use either of these options
