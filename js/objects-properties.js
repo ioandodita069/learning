09f5602d0ac0912c4c0435afffd64b958ca1ae85
ioan <ioandodita069@gmail.com>
2022-05-09 19:47:32 +0300

    john

--- js/objects-properties.js
+++ js/objects-properties.js
@@ -10,4 +10,4 @@ const myFarm = {
 // console.log(myFarm.animals.animal2);
 // console.log(myFarm.animals["animal1"]);
 console.log(myFarm["animals"]["animal3"]);
-// you can use either of these options
+// you can use either of these 
